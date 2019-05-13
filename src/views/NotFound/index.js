0aa3d99943881f4df28c995daf2350aec983b87a
Bogdan Pavlov <bugdonepavlov@gmail.com>
2019-05-13 12:37:49 +0300

    updated package

--- src/views/NotFound/index.js
+++ src/views/NotFound/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => <Content>Нуууу, нет такой страницы</Content>;
+const NotFound = () => (
+  <Content>
+    <Title>
+      <h2>Not Found</h2>
+      <StyledLink to="/">Go to home</StyledLink>
+    </Title>
+  </Content>
+);
 
 export default NotFound;
 
-const Content = styled.div``;
+const Content = styled.div`
+  background: #1976d2;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.div`
+  display: grid;
+  justify-items: center;
+
+  h2 {
+    color: #fff;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 14px;
+  padding: 6px 16px;
+  min-width: 64px;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  line-height: 1.75;
+  font-weight: 500;
+  border-radius: 4px;
+  letter-spacing: 0.02857em;
+  text-transform: uppercase;
+  cursor: pointer;
+  outline: none;
+  background: rgba(255, 255, 255, 0.6);
+`;
